4bca9257bda7793bbf5268179ca9932ef6e79316
ismaelcoulibaly <ismael.coulibaly99@gmail.com>
2023-10-22 16:51:14 -0400

    fix build

--- routes.js
+++ routes.js
@@ -5,9 +5,6 @@ const mailjet = require('node-mailjet')
     .apiConnect(process.env.MAILJET_API_KEY, process.env.MAILJET_SECRET_KEY);
 
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
-});
 
 /**
  * @swagger
