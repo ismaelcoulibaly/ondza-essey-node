84d20e5d2c3eefa1c85af268b6666a58c41829ce
ismaelcoulibaly <ismael.coulibaly99@gmail.com>
2023-10-22 16:44:35 -0400

    reverting : explicitly serving the Swagger UI

--- server.js
+++ server.js
@@ -41,9 +41,7 @@ app.use(cors(corsOptions));
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(routes);
-app.get('/swagger', (req, res) => {
-    res.send(swaggerUi.generateHTML(swaggerDocs));
-});
+app.use('/swagger', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 app.use((req, res) => {
     res.status(404).send('Page not found');
   });
