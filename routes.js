720e2e643b53f7b93b16c1bbb90f8c9d86ba72f3
ismaelcoulibaly <ismael.coulibaly99@gmail.com>
2023-10-21 22:01:26 -0400

    updated the email service

--- routes.js
+++ routes.js
@@ -1,33 +1,38 @@
 const express = require('express');
 const router = express.Router();
+const nodemailer = require('nodemailer');
+const { Reservation, Subscriber } = require('./models');
 
-const  { Reservation, Subscriber } = require('./models');
+// Configure Nodemailer transporter
+const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: 'yourgmail@gmail.com',
+        pass: 'yourapplicationpassword'
+    }
+});
 
-const sendGridMail= require('@sendgrid/mail');
-sendGridMail.setApiKey(process.env.SENDGRID_API_KEY);
-
-
-router.get('api/reservations', async (req, res) =>{
+router.get('/api/reservations', async (req, res) => {
     const reservations = await Reservation.find();
     res.json(reservations);
 });
 
-router.post('api/', async (req, res) => {
-    try{
+router.post('/api/reservations/create', async (req, res) => {
+    try {
         const reservation = new Reservation(req.body);
         await reservation.save();
 
         // Send confirmation email
         const msgToBooker = {
-            to: reservation.email,
-            from: 'ondza7@icloud.com',
+            from: 'yourgmail@gmail.com', // Sender's email address
+            to: reservation.email, // Recipient's email address
             subject: 'Reservation Confirmation',
             text: 'Your reservation has been confirmed!'
         };
-        await sendGridMail.send(msgToBooker);
+        await transporter.sendMail(msgToBooker);
 
         res.status(201).json(reservation);
-    }catch (error) {
+    } catch (error) {
         res.status(400).json({ error: error.message });
     }
 });
