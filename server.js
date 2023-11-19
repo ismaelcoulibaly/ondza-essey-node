acab4074058fda7d89f5930d4b4448c133d55c89
ismaelcoulibaly <ismael.coulibaly99@gmail.com>
2023-11-19 15:25:48 -0500

    fix build

--- server.js
+++ server.js
@@ -50,11 +50,3 @@ app.use((req, res) => {
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
-
-app.get('/', (req, res) => {
-    res.send('Hey this is my API running 🥳')
-})
-
-
-module.exports = app
-
