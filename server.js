cbce371535d598aec8f0d7487df9cb9ab69beaab
ismaelcoulibaly <ismael.coulibaly99@gmail.com>
2023-11-19 16:40:33 -0500

    added a vercel.json file

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 const corsOptions = {
-    origin: ["*", "http://localhost:3000"],
+    origin: ["*", "http://localhost:3000", "/swagger"],
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["*"],
     credentials: true
