df6f14a2b362d63d3f4940a968068d84083aa58d
ismaelcoulibaly <ismael.coulibaly99@gmail.com>
2023-11-21 15:49:42 -0500

    fixed build

--- server.js
+++ server.js
@@ -1,52 +1,64 @@
 require('dotenv').config();
-
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const routes = require('./routes');
-const swaggerJsDoc = require('swagger-jsdoc');
 const swaggerUi = require('swagger-ui-express');
+const routes = require('./routes'); // Importing your routes from routes.js
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-const corsOptions = {
-    origin: ["*", "http://localhost:3000", "/swagger"],
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["*"],
-    credentials: true
+// Swagger definition
+const swaggerDefinition = {
+    openapi: '3.0.0',
+    info: {
+        title: 'Express API for My App',
+        version: '1.0.0',
+    },
 };
 
-const swaggerOptions = {
-    definition: {
-        openapi: '3.0.0',
-        info: {
-            title: 'Reservation API',
-            version: '1.0.0',
-        },
-    },
+// Options for the swagger docs
+const options = {
+    swaggerDefinition,
+    // Paths to files containing OpenAPI definitions
     apis: ['./routes.js'],
 };
 
-const swaggerDocs = swaggerJsDoc(swaggerOptions);
+// Initialize swagger-jsdoc -> returns validated swagger spec in json format
+const swaggerSpec = swaggerJsdoc(options);
 
-// MongoDB's connection URL - replace username, password and your_cluster_url with your actual MongoDB credentials
-const dbUrl = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_CLUSTER}/?retryWrites=true&w=majority`;
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
-mongoose.connect(dbUrl, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => console.log('Connected to MongoDB!'))
-    .catch(err => console.error('Could not connect to MongoDB: ', err));
+// MongoDB Connection
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('Connected to MongoDB'))
+    .catch(err => console.error('Could not connect to MongoDB', err));
 
-app.use('/swagger', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
-app.use(cors(corsOptions));
+// Middleware
+app.use(cors());
 app.use(bodyParser.json());
-// app.use(express.static(path.join('public')));
-app.use(routes);
-app.use((req, res) => {
-    res.status(404).send('Page not found ---- test');
-  });
-  
 
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+// Swagger Documentation Setup
+const swaggerDocument = require('./swagger.json'); // Assuming you have a swagger.json
+app.use('/swagger', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+// Routes
+app.use('/api', routes);
+app.use('/', routes);
+
+// Catch-all for routes not found
+app.use((req, res, next) => {
+    res.status(404).send('Page not found');
 });
+
+// Global error handler
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something broke!');
+});
+
+// Start the server
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+module.exports = app;
