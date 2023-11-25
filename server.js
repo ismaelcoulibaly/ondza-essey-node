8a5958466513dd4f9a5afdb7a5d819a83b0f153f
ismaelcoulibaly <ismael.coulibaly99@gmail.com>
2023-11-24 22:44:57 -0500

    fixed build

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const swaggerUi = require('swagger-ui-express');
 const routes = require('./routes'); // Importing your routes from routes.js
+const swaggerJsDoc = require('swagger-jsdoc');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -25,10 +26,7 @@ const options = {
     apis: ['./routes.js'],
 };
 
-// Initialize swagger-jsdoc -> returns validated swagger spec in json format
-const swaggerSpec = swaggerJsdoc(options);
 
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 // MongoDB Connection
 mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
@@ -40,8 +38,8 @@ app.use(cors());
 app.use(bodyParser.json());
 
 // Swagger Documentation Setup
-const swaggerDocument = require('./swagger.json'); // Assuming you have a swagger.json
-app.use('/swagger', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+const swaggerDocs = swaggerJsDoc(options);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
 // Routes
 app.use('/api', routes);
