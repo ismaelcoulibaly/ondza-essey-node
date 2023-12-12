a0895de80dc24ca068b65988ca9fd9f671e6ae8c
ismaelcoulibaly <ismael.coulibaly99@gmail.com>
2023-12-12 18:52:12 -0500

    moved the whole logic in index.js

--- functions/index.js
+++ functions/index.js
@@ -1,53 +1,40 @@
-const functions = require('firebase-functions');
 const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
+const functions = require('firebase-functions');
+const mongoose = require('mongoose');
 const swaggerUi = require('swagger-ui-express');
-const routes = require('./routes'); // make sure the path is correct based on your project structure
-const swaggerJsDoc = require('swagger-jsdoc');
+const swaggerJsdoc = require('swagger-jsdoc');
+const reservationRoutes = require('./routes/reservationRoutes');
+require('dotenv').config();
 
-// Initialize Express app
 const app = express();
 
-// Swagger definition
-const swaggerDefinition = {
-    openapi: '3.0.0',
-    info: {
-        title: 'Express API for My App',
-        version: '1.0.0',
+mongoose.connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}).then(() => console.log('MongoDB Connected'))
+    .catch(err => console.error('MongoDB connection error:', err));
+
+app.use(express.json());
+
+const swaggerOptions = {
+    definition: {
+        openapi: '3.0.0',
+        info: {
+            title: 'Chef Reservation API',
+            version: '1.0.0',
+            description: 'API for managing chef reservations and newsletter subscriptions',
+        },
     },
+    apis: ['./routes/*.js'],
 };
 
-// Options for the swagger docs
-const options = {
-    swaggerDefinition,
-    apis: [require('./routes/*.js')], // Update path if necessary
-};
+const swaggerDocs = swaggerJsdoc(swaggerOptions);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
-const swaggerDocs = swaggerJsDoc(options);
+app.use('/', reservationRoutes);
 
-// Middleware
-app.use(cors());
-app.use(bodyParser.json());
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
-// Swagger Documentation Setup
-app.use('/', routes);
-app.use('/api-docs', (req, res, next) => {
-    res.setHeader('Content-Type', 'text/html');
-    next();
-}, swaggerUi.serve, swaggerUi.setup(swaggerDocs));
-app.use('/api', routes);
-
-// Catch-all for routes not found
-app.use((req, res, next) => {
-    res.status(401).send('Page not found');
-});
-
-// Global error handler
-app.use((err, req, res, next) => {
-    console.error(err.stack);
-    res.status(500).send('Something broke!');
-});
-
-// Export the app as a function
 exports.app = functions.https.onRequest(app);
+
---
+++ functions/routes/reservationRoutes.js
@@ -0,0 +1,62 @@
+const express = require('express');
+const router = express.Router();
+const { Reservation, Subscriber } = require('../models');
+const mailjet = require('node-mailjet').apiConnect(process.env.MAILJET_API_KEY, process.env.MAILJET_SECRET_KEY);
+
+// Route to get all reservations
+/**
+ * @swagger
+ * /reservations:
+ *   get:
+ *     summary: Retrieve a list of reservations
+ *     responses:
+ *       200:
+ *         description: A list of reservations
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/Reservation'
+ */
+router.get('/reservations', async (req, res) => {
+    try {
+        const reservations = await Reservation.find();
+        res.json(reservations);
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+});
+
+// Route to create a new reservation
+/**
+ * @swagger
+ * /reservation/create:
+ *   post:
+ *     summary: Create a new reservation
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/Reservation'
+ *     responses:
+ *       201:
+ *         description: Reservation created
+ *       400:
+ *         description: Error in creating reservation
+ */
+router.post('/reservation/create', async (req, res) => {
+    const reservation = new Reservation(req.body);
+    try {
+        const newReservation = await reservation.save();
+        // Send email logic goes here
+        res.status(201).json(newReservation);
+    } catch (err) {
+        res.status(400).json({ message: err.message });
+    }
+});
+
+// Add more routes as needed...
+
+module.exports = router;
