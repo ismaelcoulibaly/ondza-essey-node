29113a849bb949b2f559d32e83ad5cefe7bd259a
ismaelcoulibaly <ismael.coulibaly99@gmail.com>
2023-12-23 14:08:55 -0500

    made dateOfRequest Immutable and give a default value

--- src/index.js
+++ src/index.js
@@ -10,11 +10,11 @@
   const app = express()
   app.use(express.json())
   app.use(express.urlencoded({ extended: true }))
-
+  const dbUrl = process.env.MONGODB_URI ? `${process.env.MONGODB_URI}` : 'mongodb://localhost:27017'
   const cors = require('cors');
 
   app.use(cors());
-  mongoose.connect(process.env.MONGODB_URI, {
+  mongoose.connect(dbUrl, {
     useNewUrlParser: true,
     useUnifiedTopology: true
   }).then(() => console.log('MongoDB Connected'))
